ff4b1f6fa83a44ce868d4f9f102968f165c3ff8b
Baawaah <son.tran.be@gmail.com>
2017-03-13 11:28:54 +0100

    commiting this commit

--- src/HM.c
+++ src/HM.c
@@ -5,108 +5,96 @@
 #include <sys/sysinfo.h>
 #include <stddef.h>
 #include <unistd.h>
+#include "thomas.h"
 
-struct u_t {
-  int size;
-  double *u;
-  double *t;
-};
-
-double finiteDiff_C02(double x,double h, double (*fct)(double)){
-//  Finite Difference O(h^2)
-return (fct(x+h)-fct(x-h))/(2*h);
-}
-
-struct u_t RK4_classical(double t0,double t1,double u_0,double t_0,double dt_0,double (*fct)(double,double)){
-
-  double du = 0.0;
-  double dt = dt_0;
-  int n = 1 + (t1-t0)/dt;
-  double beta[4]  = {0.0,0.5,0.5,1.0};
-  double gamma[4] = {1.0/6.0,1.0/3.0,1.0/3.0,1.0/6.0};
-  double* u_s = (double*) malloc(sizeof(double)*n);
-  double* t_s = (double*) malloc(sizeof(double)*n);
-  //printf("Status n: %d dt: %f\n ",n,dt);
-  u_s[0] = u_0;
-  t_s[0] = t_0;
-  for(int j=1; j < n; j ++  ){
-    double u_l;
-    double t_l;
-    double u = u_s[j-1];
-    double t = t_s[j-1];
-    for(int i=0; i < 4 ; i++){
-      u_l = u_s[j-1] + beta[i]*du;
-      t_l = t   + beta[i]*dt;
-      du  = dt * fct(t_l,u_l);
-      u = u + gamma[i] * du;
-      t = t + gamma[i] * dt;
-      //printf("Status u: %f t: %f du:%f dt: %f gamma: %f\n ",u,t,du,dt,gamma[i]);
-    }
-    u_s[j] = u;
-    t_s[j] = t;
+void initU(double* U_value,double h,int N){
+  double Q     = 1.0;
+  double sigma = (2.0)*h;
+  for(int i = 0; i < N; i++){
+    U_value[i] = Q/sqrt(M_PI*sigma*sigma) * exp(-((i-N/2)*(i-N/2))/(sigma*sigma));
   }
-  struct u_t result;
-  result.size = n;
-  result.u = u_s;
-  result.t = t_s;
-  return result;
-}
-void initU(double* U_value){
-  U_value[1] = 10.0;
+  //U_value[N/2] = 10.0;
   return;
 }
 
-
-
-
 void cpyVector(double* A,double* B,int size){
-    for(int i = 0;i < size; i++) B[i] = A[i];
+    for(int i = 0; i< size; i++) B[i] = A[i];
+    return;
+}
+void sumVector(double* A,double* B,int size,double inc){
+    for(int i = 0; i< size; i++) B[i] = A[i]+inc;
+    return;
+}
+void sum_Csum_Vector(double* A,double* B,double* C,int size,double inc){
+    for(int i = 0; i< size; i++) A[i] = B[i]+C[i]*inc;
+    return;
+}
+
+double FDEE2(double* U,double h,int N,int m){
+//  Finite Difference O(h^2)
+return (U[(m+1)%N] - U[(m+N-1)%N]) / (2.0*h);
+}
+double FDEE4(double* U,double h,int N,int m){
+//  Finite Difference O(h^2)
+return (U[(m+2)%N]+8*U[(m+1)%N]-8*U[(m-1+N)%N]+U[(m-1+N)%N])/(12*h);
+}
+void solverFDEE2(double* U, double* du,double c,double h,double dt, int N){
+  for(int m = 0; m < N; m++) du[m] =  dt*(-c)*(U[(m+1)%N] - U[(m+N-1)%N]) / (2.0*h);
+  return;
+}
+void solverFDEE4(double* U, double* du,double c,double h,double dt, int N){
+  for(int m = 0; m < N; m++) du[m] =  dt*(-c)*(U[(m+2)%N]+8.0*U[(m+1)%N]-8.0*U[(m-1+N)%N]+U[(m-2+N)%N])/(12.0*h);
+  return;
+}
+void solverFDES3(double* U, double* du,double c,double h,double dt, int N){
+  for(int m = 0; m < N; m++) du[m] = dt*(-c)*(2.0*U[(m+1)%N]+3.0*U[(m)%N]-6.0*U[(m-1+N)%N]+U[(m-2+N)%N])/(12.0*h);
+  return;
 }
 
 int main(int argc,char* argv[]){
 // Init Value
-  int N = 64;
-  int Ntime = 1;
-  double h = 1.0;
-  double dt = 0.1;
+  double c = 1.0;
+  int N = 128;
+  int Ntime = 1000;
+  double h = 0.2;
+  double dt = 0.2;
   double* U     = calloc(N    ,sizeof(double));
-  double* t     = calloc(Ntime,sizeof(double));
-  double* dudx  = calloc(N    ,sizeof(double));
-  double* dudt  = calloc(N    ,sizeof(double));
+  //double* t     = calloc(N,sizeof(double));
+  //double* dudx  = calloc(N    ,sizeof(double));
+  double* du  = calloc(N ,sizeof(double));
 
-  //int n = 1 + (t1-t0)/dt;
+  double* Us = calloc(N , sizeof(double));
+  double* Uloc = calloc( N , sizeof(double));
+  //double* tloc = calloc( N , sizeof(double));
+
   double beta[4]  = {0.0,0.5,0.5,1.0};
   double gamma[4] = {1.0/6.0,1.0/3.0,1.0/3.0,1.0/6.0};
 // Init Initial Condition
-  initU(U);
-
+  initU(U,h,N);
+//for(int j = 0; j<N ;j++) printf("%d %f\n",j,U[j]);
 //for(int j = 0; j<N ;j++) printf("U: %f dudx: %f\n",U[j],dudx[j]);
-
-
-// Solving The Matrix
-  for(int k = 0; k<N; k++){
-    dudx[k] = (U[(k+1)%N] - U[(k+N-1)%N]) / (2.0*h);
-  }
-
 // Time integrating
   for(int tn = 0; tn < Ntime ; tn++){ // Time Loop
-    for(int n = 0; tn < N , n++){ // Space Loop
-      double* Us = calloc( N , sizeof(double));
-      for(int k = 0; k < 4 ; k++){ // RK4 Loop
-        Uk = Us + beta[k]*dudt; TOODOOOOOO HEEEEREEEE
-        tp = t  + gamma[k]*dt;
-        dudt[n] = dt*FX TOO DOOOO;
-        U = U + gamma[k]*dudt;
-        t = t + gamma[k]*dt;
-      }
-    }
+        cpyVector(U,Us,N);
+        // Solving The Matrix
+        for(int k = 0; k < 4 ; k++){ // RK4 Loop
+            sum_Csum_Vector(Uloc,Us,du,N,beta[k]);
+            //tloc[m] = t[m]  + gamma[k]*dt;
+            solverFDES3(Uloc,du,c,h,dt,N);
+            sum_Csum_Vector(U,U,du,N,gamma[k]);
+            //t[m] = t[m] + gamma[k]*dt;
+        }
   }
+  //printf("ITERATION \n");
+  for(int j = 0; j<N ;j++) printf("%d %f \n",j,U[j]);
 
-
-for(int j = 0; j<N ;j++) printf("U: %f dudx: %f \n",U[j],dudx[j]);
-printf("%d %d\n",4%4,(4+1)%4);
 free(U);
-free(dudx);
+//free(t);
+//free(dudx);
+free(du);
+free(Us);
+free(Uloc);
+//free(tloc);
 return 1;
 
 
