595bc17806a055de23b9b4ad41d01504aad24d9e
Julien Demey <juliendemey@156.145-4-62.wifi-dyn.isp.proximus.be>
2017-04-26 14:52:26 +0200

    time integration in progress - cfd.h need to be updated

--- Project/src/cfd_integrator.c
+++ Project/src/cfd_integrator.c
@@ -1,25 +1,35 @@
 #include "cfd.h"
 
-void scalar_rhs(struct _problem* Problem, int i, int j, double du){
+double scalar_rhs_conv(struct _problem* Problem, int i, int j){
     double omega_loc = (*Problem).omega;
     double h_loc = (*Problem).h;
-    double domdy,dom2dy2,domdx,dom2dx2 ;
-
-    domdy = (omega_loc[i][j+1]-omega_loc[i][j-1])/(2*h_loc);
-    dom2dy2 = (omega_loc[i][j+1]-2*omega_loc[i][j]+omega_loc[i][j-1])/h_loc^2;
+    double domdx,domdy;
 
     domdx = (omega_loc[i+1][j]-omega_loc[i-1][j])/(2*h_loc);
-    dom2dx2 = (omega_loc[i+1][j]-2*omega_loc[i][j]+omega_loc[i-1][j])/h_loc^2;
+    domdy = (omega_loc[i][j+1]-omega_loc[i][j-1])/(2*h_loc);
 
-    du = domdy + dom2dy2 + domdx + dom2dx2;
+    return (domdy + domdx);
+
+}
+
+double scalar_rhs_diff(struct _problem* Problem, int i, int j){
+    double omega_loc = (*Problem).omega;
+    double h_loc = (*Problem).h;
+    double dom2dx2,dom2dy2 ;
+
+    dom2dx2 = (omega_loc[i+1][j]-2*omega_loc[i][j]+omega_loc[i-1][j])/h_loc^2;
+    dom2dy2 = (omega_loc[i][j+1]-2*omega_loc[i][j]+omega_loc[i][j-1])/h_loc^2;
+
+    return (dom2dy2 + dom2dx2);
 
 }
 
 void first_iteration_omega(struct _problem* Problem, double du_old){
     for(int i=0; i<(*Problem).Nx; i++){
         for(int j = 0; j < (*Problem).Ny ; j++){
-            du_old=scalar_rhs((*Problem),i,j,du);
-            (*Problem).omega[i][j]=(*Problem).omega[i][j]+du_old*(*Problem).dt;
+            du_old_conv=scalar_rhs_conv((*Problem),i,j);
+            du_old_diff=scalar_rhs_diff((*Problem),i,j);
+            (*Problem).omega[i][j]=(*Problem).omega[i][j]+(*Problem).dt*(du_old_conv+du_old_diff); // EE for both at the first time step
         }
     }
 }
@@ -30,8 +40,9 @@ void integration_omega(struct _problem* Problem){
 
     for(int i=0; i<(*Problem).Nx; i++){
         for(int j = 0; j < (*Problem).Ny ; j++){
-            du=scalar_rhs((*Problem),i,j,du);
-            (*Problem).omega[i][j]=(*Problem).omega[i][j]+dt/2*(3*du-du_old);
+            du_conv=scalar_rhs_conv((*Problem),i,j);
+            du_diff=scalar_rhs_diff((*Problem),i,j);
+            (*Problem).omega[i][j]=(*Problem).omega[i][j]+(*Problem).dt*(1/2*(3*du_conv-du_old)+du_diff);
         }
     }
 
