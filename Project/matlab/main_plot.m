b012bbf2542bce26b846e8fb7aee832e4c0a9416
Son Tran <Son.Tran.be@gmail.com>
2017-05-24 22:42:38 +0200

    yay..

--- Project/matlab/main_plot.m
+++ Project/matlab/main_plot.m
@@ -2,23 +2,36 @@
 
 plot_all = 0;
 plot_R   = 0;
-plot_diag= 1;
-plot_pres= 1;
+plot_diag= 0;
+plot_pres= 0;
+plot_fin = 1;
 %%
 load('colormapsavefile.mat')
 %t = [0 25 37 50 62 75 87 99];
-t = [0 50 99 149 200];
+%t = [0 50 99 149 200];
 %t =  [0 24];
 %t = 99;
 %t = 100;
+%t = [ 5 24 34 54 74 84 100]
+%t= [66 70 73 76 100]
+t = 200
 for k = 1 : length(t); 
-    file_omega = sprintf('../data_server_4/CFD_omega_%d.txt',t(k));
-    file_psi   = sprintf('../data_server_4/CFD_psi_%d.txt',t(k));
-    file_u     = sprintf('../data_server_4/CFD_u_%d.txt',t(k));
-    file_v     = sprintf('../data_server_4/CFD_v_%d.txt',t(k));
-    file_R     = sprintf('../data_server_4/CFD_R_%d.txt',t(k));
-    file_P     = sprintf('../data_server_4/CFD_P_%d.txt',t(k));
-    file_R_pres= sprintf('../data_server_4/CFD_R_pres_%d.txt',t(k));
+    file_omega = sprintf('../data_server_6_t200_R150_h0015/CFD_omega_%d.txt',t(k));
+    file_psi   = sprintf('../data_server_6_t200_R150_h0015/CFD_psi_%d.txt',t(k));
+    file_u     = sprintf('../data_server_6_t200_R150_h0015/CFD_u_%d.txt',t(k));
+    file_v     = sprintf('../data_server_6_t200_R150_h0015/CFD_v_%d.txt',t(k));
+    file_R     = sprintf('../data_server_6_t200_R150_h0015/CFD_R_%d.txt',t(k));
+    file_P     = sprintf('../data_server_6_t200_R150_h0015/CFD_P_%d.txt',t(k));
+    file_R_pres= sprintf('../data_server_6_t200_R150_h0015/CFD_R_pres_%d.txt',t(k));
+
+%     file_omega = sprintf('../data/CFD_omega_%d.txt',t(k));
+%     file_psi   = sprintf('../data/CFD_psi_%d.txt',t(k));
+%     file_u     = sprintf('../data/CFD_u_%d.txt',t(k));
+%     file_v     = sprintf('../data/CFD_v_%d.txt',t(k));
+%     file_R     = sprintf('../data/CFD_R_%d.txt',t(k));
+%     file_P     = sprintf('../data/CFD_P_%d.txt',t(k));
+%     file_R_pres= sprintf('../data/CFD_R_pres_%d.txt',t(k));
+
     omega (:,:,k) = load(file_omega);
     psi   (:,:,k) = load(file_psi);
     u     (:,:,k) = load(file_u);
@@ -29,7 +42,8 @@ for k = 1 : length(t);
     R_pres(:,:,k) = load(file_R_pres);
     end
 end
-Diag = load('../data_server_4/CFD_DIAG.txt');
+ Diag = load('../data_server_6_t200_R150_h0015/CFD_DIAG.txt');
+% Diag = load('../data/CFD_DIAG.txt');
 %% 
 k = 1;
 [SY SX] = size(omega(:,:,1));
@@ -83,19 +97,27 @@ end
 end
 %%
 if plot_diag == 1
-sample = [1 : 10 :length(Diag(:,1))];
+sample = [1 : 600 :length(Diag(:,1))];
 figure;
 RE_HL  = 10 * ones(length(sample));
 RE_HOL = 5  * ones(length(sample));
-plot(Diag(sample,1),Diag(sample,2),Diag(sample,1),Diag(sample,3),Diag(sample,1),RE_HL ,'--',Diag(sample,1),RE_HOL,'--');
+%hold on;
+plot(Diag(sample,1),Diag(sample,2),Diag(sample,1),Diag(sample,3),Diag(sample,1),RE_HL,'--',Diag(sample,1),RE_HOL,'--');
+%plot(Diag(sample,1),RE_HL,Diag(sample,1),RE_HOL);
+%hold off;
 %plot(Diag(:,1),Diag(:,3),Diag(:,1),RE_HOL,'--');
 legend('Re_h','Re_h_\omega','Re_h Limit','Re_h_\omega Limit');
+
+title('Diagnostic Re_h and Re_h_\omega')
+xlabel('\tau - Adimensional time')
+axis([0,1,0,11]);
 %legend('Re_h_\omega','Re_h_\omega Limit');
-figure;
-plot(Diag(sample,1),Diag(sample,3));
+%figure;
+%plot(Diag(sample,1),Diag(sample,4));
 end    
 %% 
 if plot_pres == 1
+k = 1
 figure;
 imP1 = imagesc(P(:,:,k));
 axis equal; axis xy;
@@ -114,14 +136,78 @@ colorbar;
 title('R - Residu')    
 end    
 %%
-
-k = 5;
+% figure;
+% imP2 = imagesc(omega(:,:,2));
+% axis equal; axis xy;
+% axis([0,SX,0,SY]);
+% caxis([-0.001 0.001])
+% colormap(myColormap);
+%%
+if plot_fin == 1
+k = 1;
 figure;
 UV = sqrt(u(:,:,k).*u(:,:,k) + v(:,:,k).*v(:,:,k))/(1.5e-4);
-imagesc(UV);
+x = linspace(0,SX,SX);
+y = linspace(0,SY,SY);
+imagesc(x,y,UV);
+
 axis equal; axis xy;
 axis([0,SX,0,SY]);
+%xticklabels = 0:20:100;
+%caxis([0.0 1]);
 colormap(myColormap);
 colorbar;
 title('Velocity Field');
+
+
+L=15;
+H=1;
+Ls=5*H;
+Hs=1/2;
+for k = int64([H (Ls-H) (Ls-H/4) Ls (Ls+H/4) (Ls+H) (L-H)]*(1/0.015))  
+figure
+plot(abs(u(:,k,1)/1.5e-4),linspace(0,1,SY))
+title('Dimensionless Horizontal Velocity Profile')
+ylabel('y/H')
+xlabel('uH/Q_0')
+% s=sprintf('adim_velocity_field_%d',k);
+% print(s,'-depsc','-tiff')   
+% figure
+% plot(abs(omega(:,k,1)/1.5e-4),linspace(0,1,SY))
+% title('Dimensionless Horizontal Velocity Profile')
+% ylabel('y/H')
+% xlabel('\omega H^2/Q_0')
+% % s=sprintf('adim_vorticity_field_%d',k);
+% % print(s,'-depsc','-tiff')  
+
+figure;
+%subplot(2,1,1);
+im4 = imagesc(P(:,:,1)/(1.5e-4*1.5e-4));
+axis equal; axis xy;
+axis([0,SX,0,SY]);
+caxis([min(min(P(:,:,k)/(1.5e-4*1.5e-4))) max(max(P(:,:,k)/(1.5e-4*1.5e-4)))]);
+colormap(myColormap);
+colorbar;
+title('Dimensionless Pressure')
+% s=sprintf('pressure_%d',k);
+% print(s,'-depsc','-tiff')
+figure;
+%subplot(2,1,2);
+im5 = imagesc(B(:,:,1)/(1.5e-4*1.5e-4));
+axis equal; axis xy;
+axis([0,SX,0,SY]);
+caxis([min(min(B(:,:,k)/(1.5e-4*1.5e-4))) max(max(B(:,:,k)/(1.5e-4*1.5e-4)))]);
+colormap(myColormap);
+colorbar;
+title('Total Pressure')
+% s=sprintf('pressure_tot_%d',k);
+% print(s,'-depsc','-tiff')
+s=sprintf('plot_backward_pressures_tau2_%d',k);
+print(s,'-depsc','-tiff')
+end
+
+
+
+end
+
 %%
